055c8c40e8f7817f1dad2660736bee795b251ee4
Edgar Sarkisyan <hxxperr999@gmail.com>
2023-07-19 21:03:41 +0300

    Добавьте файлы проекта.

---
+++ WebAppTest/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppTest.Models;
+
+namespace WebAppTest.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebAppTest/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppTest.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        [StringLength(30, ErrorMessage = "Текст менее 30 символов")]
+        public string Message { get; set; }
+
+    }
+}
